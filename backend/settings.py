bb83464e3fdb6d03175e54f7a48238896b79ae54
Selim Başpınar <56540379+SelimBaspinar@users.noreply.github.com>
2022-01-09 00:27:12 +0300

    corsorigins

--- backend/settings.py
+++ backend/settings.py
@@ -133,7 +133,8 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+CORS_ORIGIN_WHITELIST = [
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
 
