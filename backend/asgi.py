7689ae1b50e464e0498e778e28dc6d92bb0d6525
Selim Başpınar <56540379+SelimBaspinar@users.noreply.github.com>
2022-01-09 15:54:15 +0300

    Update asgi.py

--- backend/asgi.py
+++ backend/asgi.py
@@ -7,7 +7,6 @@ from django.core.asgi import get_asgi_application
 import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-django.setup()
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
