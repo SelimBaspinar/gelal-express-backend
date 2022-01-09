0030004e270a29b451b9fffaf3f1b240920f67ed
Selim Başpınar <56540379+SelimBaspinar@users.noreply.github.com>
2022-01-09 03:14:23 +0300

    Update asgi.py

--- backend/asgi.py
+++ backend/asgi.py
@@ -9,16 +9,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import get_default_application
-from django.conf.urls import url
-import django
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-django.setup()
-application=get_default_application()
+import os
 
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+application = get_asgi_application()
 
 
